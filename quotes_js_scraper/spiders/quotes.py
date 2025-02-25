934c8ae3195be9d19259b95d07e8954669120017
PixelVans <evansmemba001@gmail.com>
2025-02-25 21:51:05 +0300

    page scrolling functionality added

--- quotes_js_scraper/spiders/quotes.py
+++ quotes_js_scraper/spiders/quotes.py
@@ -6,35 +6,45 @@ class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
     def start_requests(self):
-        url = 'http://quotes.toscrape.com/js/'
+        url = 'http://quotes.toscrape.com/scroll'
         yield scrapy.Request(
             url,
             meta={
                 "playwright": True,
                 "playwright_include_page": True,
                 "playwright_page_methods": [
-                    PageMethod("wait_for_selector", "div.quote") 
+                    PageMethod("wait_for_selector", "div.quote"),
+                    PageMethod("evaluate", """
+                        async () => {
+                            let previousHeight = 0;
+                            while (document.body.scrollHeight > previousHeight) {
+                                previousHeight = document.body.scrollHeight;
+                                window.scrollBy(0, document.body.scrollHeight);
+                                await new Promise(resolve => setTimeout(resolve, 2000));  // Wait 2s
+                            }
+                        }
+                    """) 
                 ],
             },
             errback=self.errback
         )
 
-    def parse(self, response):
-        page = response.meta['playwright_page']
-        await page.close()
-        
+    async def parse(self, response):
+        page = response.meta["playwright_page"]
         quote_item = QuoteItem()
         for quote in response.css("div.quote"):
             quote_item["text"] = quote.css("span.text::text").get() 
             quote_item["author"] = quote.css("small.author::text").get()
             quote_item["tags"] = quote.css("div.tags a.tag::text").getall()
             yield quote_item
-            
-       next_page = response.css('.next>a ::attr(href)').get()
-       if next_page is not None:
-           next_page_url = 'http://quotes.toscrape.com' + next_page
-           yield scrapy.Request(
-                url,
+        
+        await page.close()
+       
+        next_page = response.css('.next>a ::attr(href)').get()
+        if next_page is not None:
+            next_page_url = 'http://quotes.toscrape.com' + next_page
+            yield scrapy.Request(
+                next_page_url, 
                 meta={
                     "playwright": True,
                     "playwright_include_page": True,
@@ -43,7 +53,7 @@ class QuotesSpider(scrapy.Spider):
                     ],
                 },
                 errback=self.errback
-        )    
+            )  
 
     async def errback(self, failure):
         page = failure.request.meta["playwright_page"]
